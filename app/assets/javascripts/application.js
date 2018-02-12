a99baf588e00e2be4b916e738e7d141d61ed8ac5
tayyab841 <tayyab841@gmail.com>
2018-02-12 14:43:55 +0000

    javascript issues

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,96 +28,102 @@
 // 
 
 $(document).on('turbolinks:load', function() {
-    $(document).ready(function () {
-    $('#input-email').on('focus', function(){
-        $('.label-email').addClass('label-after');
-        $('#input-email').css('border-color','#4771FA');
+    $(document).ready(function() {
+        $('#input-email').on('focus', function() {
+            $('.label-email').addClass('label-after');
+            $('#input-email').css('border-color', '#4771FA');
+        });
+        $('#input-name').on('focus', function() {
+            $('.label-name').addClass('label-after');
+            $('#input-name').css('border-color', '#4771FA');
+        });
+        $('#input-pass').on('focus', function() {
+            $('.label-pass').addClass('label-after');
+            $('#input-pass').css('border-color', '#4771FA');
+        });
+        $('#input-pass-c').on('focus', function() {
+            $('.label-pass-c').addClass('label-after');
+            $('#input-pass-c').css('border-color', '#4771FA');
+        });
+        $('#input-email').on('focusout', function() {
+            if ($('#input-email').val() == '') {
+                $('.label-email').removeClass('label-after');
+                $('#input-email').css('border-color', '#e5e6e7');
+
+            } else if ($('#input-email').val() != '') {
+                $('.label-email').addClass('label-after');
+                $('#input-email').css('border-color', '#4771FA');
+            }
+
+        });
+        $('#input-name').on('focusout', function() {
+            if ($('#input-name').val() == '') {
+                $('.label-name').removeClass('label-after');
+                $('#input-name').css('border-color', '#e5e6e7');
+
+            } else if ($('#input-name').val() != '') {
+                $('.label-name').addClass('label-after');
+                $('#input-name').css('border-color', '#4771FA');
+            }
+
+        });
+        $('#input-pass').on('focusout', function() {
+            if ($('#input-pass').val() == '') {
+                $('.label-pass').removeClass('label-after');
+                $('#input-pass').css('border-color', '#e5e6e7');
+
+            } else if ($('#input-pass').val() != '') {
+                $('.label-pass').addClass('label-after');
+                $('#input-pass').css('border-color', '#4771FA');
+            }
+
+        });
+        $('#input-pass-c').on('focusout', function() {
+            if ($('#input-pass-c').val() == '') {
+                $('.label-pass-c').removeClass('label-after');
+                $('#input-pass-c').css('border-color', '#e5e6e7');
+
+            } else if ($('#input-pass-c').val() != '') {
+                $('.label-pass-c').addClass('label-after');
+                $('#input-pass-c').css('border-color', '#4771FA');
+            }
+
+        });
     });
-    $('#input-name').on('focus', function(){
-        $('.label-name').addClass('label-after');
-        $('#input-name').css('border-color','#4771FA');
-    });
-    $('#input-pass').on('focus', function(){
-        $('.label-pass').addClass('label-after');
-        $('#input-pass').css('border-color','#4771FA');
-    });
-    $('#input-pass-c').on('focus', function(){
-        $('.label-pass-c').addClass('label-after');
-        $('#input-pass-c').css('border-color','#4771FA');
-    });
-    $('#input-email').on('focusout', function(){
-    if ( $('#input-email').val() == '' ) {
-        $('.label-email').removeClass('label-after');
-        $('#input-email').css('border-color','#e5e6e7');
-            
-    }
-    else if ( $('#input-email').val() != '' ) {
-       $('.label-email').addClass('label-after');
-       $('#input-email').css('border-color','#4771FA');
-    }
-    
-    });
-    $('#input-name').on('focusout', function(){
-    if ( $('#input-name').val() == '' ) {
-        $('.label-name').removeClass('label-after');
-        $('#input-name').css('border-color','#e5e6e7');
-            
-    }
-    else if ( $('#input-name').val() != '' ) {
-       $('.label-name').addClass('label-after');
-       $('#input-name').css('border-color','#4771FA');
-    }
-    
-    });
-    $('#input-pass').on('focusout', function(){
-    if ( $('#input-pass').val() == '' ) {
-        $('.label-pass').removeClass('label-after');
-        $('#input-pass').css('border-color','#e5e6e7');
-            
-    }
-    else if ( $('#input-pass').val() != '' ) {
-       $('.label-pass').addClass('label-after');
-       $('#input-pass').css('border-color','#4771FA');
-    }
-    
-    });
-    $('#input-pass-c').on('focusout', function(){
-    if ( $('#input-pass-c').val() == '' ) {
-        $('.label-pass-c').removeClass('label-after');
-        $('#input-pass-c').css('border-color','#e5e6e7');
-            
-    }
-    else if ( $('#input-pass-c').val() != '' ) {
-       $('.label-pass-c').addClass('label-after');
-       $('#input-pass-c').css('border-color','#4771FA');
-    }
-    
-    });
-});
 
 
-$('.dataTables-example').DataTable({
-    pageLength: 25,
-    responsive: true,
-    dom: '<"html5buttons"B>lTfgitp',
-    buttons: [
-        { extend: 'copy'},
-        {extend: 'csv'},
-        {extend: 'excel', title: 'ExampleFile'},
-        {extend: 'pdf', title: 'ExampleFile'},
+    $('.dataTables-example').DataTable({
+        pageLength: 25,
+        responsive: true,
+        dom: '<"html5buttons"B>lTfgitp',
+        buttons: [{
+                extend: 'copy'
+            },
+            {
+                extend: 'csv'
+            },
+            {
+                extend: 'excel',
+                title: 'ExampleFile'
+            },
+            {
+                extend: 'pdf',
+                title: 'ExampleFile'
+            },
 
-        {extend: 'print',
-         customize: function (win){
-                $(win.document.body).addClass('white-bg');
-                $(win.document.body).css('font-size', '10px');
+            {
+                extend: 'print',
+                customize: function(win) {
+                    $(win.document.body).addClass('white-bg');
+                    $(win.document.body).css('font-size', '10px');
 
-                $(win.document.body).find('table')
+                    $(win.document.body).find('table')
                         .addClass('compact')
                         .css('font-size', 'inherit');
-        }
-        }
-    ]
+                }
+            }
+        ]
 
-});
+    });
 
 });
