16ec703340da5121ac8a35c47db28bd936579212
a17100262 <maueezahmed@gmail.com>
2018-04-03 10:27:46 +0000

    changes sidekiq.rb file

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,8 @@
+redis_url = ENV['REDISTOGO_URL'] || 'redis://localhost:6379'
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379' }
+  config.redis = { url: redis_url  }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379'}
+  config.redis = { url: redis_url}
 end
